6cff7446a10ae8a65a3ca535e3a8ac751d6410cf
bmagnantb <bmagnantb@gmail.com>
2015-05-16 14:20:56 -0500

    we may need this index.html

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ var express = require('express')
 
 var app = express()
 
-app.use(express.static(path.join(__dirname + '')))
+app.use(express.static(path.join(__dirname + '/dist')))
 
 app.listen(process.env.PORT || '3000', function() {
 	console.log('server up yo')
